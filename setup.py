5820b08951744981857746fa3da62eb7360d95d2
Melvin <MelvinKool@users.noreply.github.com>
2017-01-27 15:34:07 -0500

    added pytest to dependencies

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
-#import os
+import os
 
-#with open("requirements.txt") as reqfile:
-#    required = reqfile.read().splitlines()
+with open("requirements.txt") as reqfile:
+    required = reqfile.read().splitlines()
 
 setup(
         name="reverse-proxy",
         description="A reverse proxy that is used for load balancing http traffic between web servers",
         url="https://github.com/MelvinKool/reverse-proxy",
         packages=find_packages(),
-        #install_requires=required
+        install_requires=required
 )
 
